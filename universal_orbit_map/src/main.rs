44a966b359126b604df458d3f13647181a185e49
Keith L <keithl@radontech.xyz>
2020-05-02 18:34:52 -0400

    day 6 first attempt

--- universal_orbit_map/src/main.rs
+++ universal_orbit_map/src/main.rs
@@ -7,4 +7,48 @@ fn main() {
     let orbits: Vec<&str> = file.split_whitespace().collect();
 
     println!("{:?}", orbits);
+
+    println!("Total orbits = {}", ind_orbits(orbits));
+}
+
+fn ind_orbits(orbits: Vec<&str>) -> u64{
+    let mut all_orbits: Vec<Vec<&str>> = Vec::new();
+    for i in &orbits{
+        println!("Determing what value is attached to COM");
+        let test_val = &i[0..3];
+        println!("Testing {}", test_val);
+        println!("Against {}", "COM");
+        if test_val == "COM"{
+            println!("Match found. It is {}. Adding to vector.", &i[4..]);
+            all_orbits[0].push(&i[4..]);
+            println!("The value was succesfully added to the vector!");
+        }
+    }
+    
+    //println!("{:?}", all_orbits);
+    
+    for i in &orbits{
+        let all_orbits_len = all_orbits.len();
+        let index = all_orbits.len() - 1;
+
+        for j in 0..index{
+            let last_index: usize = all_orbits[j].len() - 1;
+            let test_vec = &all_orbits[j];
+            let test_str = test_vec[last_index];
+
+            if &i[0..3] == test_str{
+                all_orbits[all_orbits_len] = all_orbits[j].to_vec();
+                all_orbits[all_orbits_len].push(&i[5..8]);
+            }
+        }
+        println!("{:?}", all_orbits);
+    }
+
+    let mut sum: u64 = 0;
+
+    for k in all_orbits{
+        sum += k.len() as u64 - 1
+    }
+
+    sum
 }
